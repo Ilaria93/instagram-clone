cba1f963b0c8e01d6aa0553eaf97d4f74b6b1eec
Ilaria <kiraleto@gmail.com>
2022-05-30 20:15:37 +0200

    add stories content

--- src/components/LeftSidebar.js
+++ src/components/LeftSidebar.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Card from './Card';
+import Stories from './Stories';
 
 export default function LeftSidebar(){
     return(
         <div>
+            <Stories></Stories>
             <Card></Card>
             <Card></Card>
             <Card></Card>
---
+++ src/components/Stories.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import './styles/stories.css';
+
+export default function Stories(){
+    return(
+        <div className="stories__content">
+            <div className="first_stories">
+                <Avatar alt="" src="" ></Avatar>
+                <p>Son Goku</p>
+            </div>
+            <div className="first_stories">
+                <Avatar alt="" src="" ></Avatar>
+                <p>Trunks</p>
+            </div>
+            <div className="first_stories">
+                <Avatar alt="" src="" ></Avatar>
+                <p>Son Goan</p>
+            </div>
+            <div className="first_stories">
+                <Avatar alt="" src="" ></Avatar>
+                <p>Bulma</p>
+            </div>
+            <div className="first_stories">
+                <Avatar alt="" src="" ></Avatar>
+                <p>Son Goten</p>
+            </div>
+            <div className="first_stories">
+                <Avatar alt="" src="" ></Avatar>
+                <p>Pan</p>
+            </div>
+            <div className="first_stories">
+                <Avatar alt="" src="" ></Avatar>
+                <p>Bra</p>
+            </div>
+            <div className="first_stories">
+                <Avatar alt="" src="" ></Avatar>
+                <p>C-18</p>
+            </div>
+
+        </div>
+    )
+}
